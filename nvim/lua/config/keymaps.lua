9998c0852bc167d4a76d6cc76768405658111b38
OkunamiShouta <shota.okunami@proxima-ai-tech.com>
2024-06-06 05:04:56 -0400

    2nd

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,17 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "<C-c>", "<ESC>", { noremap = true, silent = true })
+
+LazyVim.safe_keymap_set("i", "<A-CR>", 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false,
+})
+vim.g.copilot_no_tab_map = true
+
+LazyVim.safe_keymap_set({ "n", "i", "v", "t" }, "<A-w>", "<Up>")
+LazyVim.safe_keymap_set({ "n", "i", "v", "t" }, "<A-s>", "<Down>")
+LazyVim.safe_keymap_set({ "n", "i", "v", "t" }, "<A-a>", "<Left>")
+LazyVim.safe_keymap_set({ "n", "i", "v", "t" }, "<A-d>", "<Right>")
+
+LazyVim.safe_keymap_set({ "n", "i", "v", "t" }, "<A-q>", "<esc>")
+LazyVim.safe_keymap_set({ "n", "i", "v", "t" }, "<A-t>", "<cmd>q<CR>")
